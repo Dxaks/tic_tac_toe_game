9ca5bfa12bffec91ec80ba43543ecc6928890ba5
Zakariya Auwal <192784011+Dxaks@users.noreply.github.com>
2025-10-27 00:47:52 +0100

    create isWin function for the gameController create score getter function for each player object create divs for player1 and player2 score

--- game_logic/logic.js
+++ game_logic/logic.js
@@ -22,17 +22,14 @@ const gameBoard = (() => {
     
        const targetCell = board[rowNumber][columnNumber]
         // condition to validate if the spot is empty
-       if (targetCell.getCellValue() === 0) {
-        targetCell.addCellValue(player);
-       } else {console.log('target cell already taken');
-        return;
-       }
+        if(targetCell.getCellValue() !== 0) return;
+       if (targetCell.getCellValue() === 0) {targetCell.addCellValue(player);} 
 }
     // console log to test the print board
     const printBoard = () => {
 
         const viewBoard = board.map(row => row.map(cell => cell.getCellValue()));
-        console.log(viewBoard)
+        return viewBoard;
     }
  
 return {
@@ -61,12 +58,16 @@ function cell() {
 function gameController(playerOneName = 'Player one',
 playerTwoName = 'player two') {
 
-    const player = [{name: playerOneName, token: 1}, {name: playerTwoName, token: 2}];
+    const player = [
+        {name: playerOneName, token: 1, score: 0}, 
+        {name: playerTwoName, token: 2, score: 0}
+    ];
 
     let activePlayer = player[0];
 
 // getter function for active player
     const getActivePlayer = () => activePlayer;
+    
 // function to change turn for each player
     const switchTurn = () => {
         if (activePlayer === player[0]) {
@@ -74,20 +75,59 @@ playerTwoName = 'player two') {
     } else {
         activePlayer = player[0]
     }
+    console.log(getActivePlayer())
 }
+
+const getPlayer1 = () => player[0];
+const getPlayer2 = () => player[1];
+
+    function isWin(player) {
+        const board = gameBoard.printBoard();
+        console.log(board);
+        for (let i = 0; i < board.length; i++) {
+            // horizontal check
+            if (board[i][0] !== 0 && board[i][0] === board[i][1] && board[i][1] === board[i][2]) {
+                player.score++;
+                return console.log(`${player.name} has ${player.score}`);       
+            }
+        }
+        for (let j = 0; j < board.length; j++) {
+            // vertical check
+            if (board[0][j] !== 0 && board[0][j] === board[1][j] && board[1][j] === board[2][j]) {
+                player.score++;
+                console.log(`${player.name} has ${player.score}`)
+                return;
+            }
+        }
+            // diagonal check_left
+        if (board[0][0] !== 0 && board[0][0] === board[1][1] && board[1][1] === board[2][2]) {
+            player.score++;
+            console.log(`${player.name} has ${player.score}`)
+            return
+        }
+            // diagonal check_right
+        if (board[0][2] !== 0 && board[0][2] === board[1][1] && board[1][1] === board[2][0]) {
+            console.log(`${player.name} has ${player.score}`)
+            player.score++;
+            return
+        }
+    }
 // function to switch play round
- const playRound = (row, column) => {
+    const playRound = (row, column) => {
 
-    gameBoard.dropToken(row, column, getActivePlayer().token);
-    gameBoard.printBoard()
+        gameBoard.dropToken(row, column, getActivePlayer().token);
+        gameBoard.printBoard()
 
-    switchTurn()
-    getActivePlayer()
- }
+        isWin(getActivePlayer())
+        switchTurn()
+        getActivePlayer()
+    }
 
  return {
     playRound,
-    getActivePlayer
+    getActivePlayer,
+    getPlayer1,
+    getPlayer2
  }
 
 }
@@ -96,18 +136,21 @@ playerTwoName = 'player two') {
 function screenController() {
 
     const game = gameController();
-    console.log(game.getActivePlayer().name)
-
     const board = gameBoard.getBoard();
-// html el. for player turn and buttons container
+// html el. for player turn, player score and buttons container
     const playerTurn = document.querySelector('.playerTurnDiv');
     const cellDiv = document.querySelector('.cellDiv');
+    const scoreDiv1 = document.querySelector('.player1')
+    const scoreDiv2 = document.querySelector('.player2')
 // update screen function
     const updateScreen = () => {
 // new inner el. content for each cellDiv update
         cellDiv.innerHTML = '';
+        const activePlayer = game.getActivePlayer();
+        const player1 = game.getPlayer1();
+        const player2 = game.getPlayer2();
 
-        playerTurn.textContent = `${game.getActivePlayer().name} turn`;
+        playerTurn.textContent = `${activePlayer.name} turn`;
 
         board.forEach((row, rowIndex) => {
             row.forEach((col, colIndex) => {
@@ -118,7 +161,7 @@ function screenController() {
                 cellBtn.dataset.row = rowIndex;
                 cellBtn.dataset.column = colIndex;
                 cellDiv.appendChild(cellBtn);
-// logic to update the button text content for each token '0, 1, 2,
+                // logic to update the button text content for each token '0, 1, 2,
                 if (col.getCellValue() === 0) {
                     cellBtn.textContent = '';
                 } else if (col.getCellValue() === 1){
@@ -126,11 +169,12 @@ function screenController() {
                 } else {
                     cellBtn.textContent = 'O';
                 }
-
             })
         })
-    }
 
+        scoreDiv1.textContent = `Score: ${player1.score}`;
+        scoreDiv2.textContent = `Score: ${player2.score}`;
+    }
 // click handler function to update playRound with the datasets of the target button as an argument
     const clickHandler = (e) => {
 
@@ -138,11 +182,10 @@ function screenController() {
         let selectedColumn = e.target.dataset.column;
 
         console.log(selectedRow, selectedColumn)
-// turn off for accidential click 
+        // turn off for accidential click 
         if ((!selectedRow || !selectedColumn)) return;
 
         game.playRound(selectedRow, selectedColumn);
-
         updateScreen();
 
     }
